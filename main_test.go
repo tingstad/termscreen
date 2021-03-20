98c04f26b7e19f428e629a71f75c902e49aabeee
Richard Tingstad <richard.tingstad@amedia.no>
2021-03-20 10:42:32 +0100

    TCR

--- main_test.go
+++ main_test.go
@@ -20,7 +20,7 @@ func (r *TestReader) ReadString(delim byte) (string, error) {
 }
 
 func TestOneLine(t *testing.T) {
-	lines := CaptureReader(strings.NewReader("hello\n"))
+	lines := CaptureReaderNew(strings.NewReader("hello\n"))
 
 	got := strings.Join(lines, "")
 	if got != "hello\n" {
