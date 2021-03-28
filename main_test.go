2bee3db5c6cfd65d85d78efd3a2ae7f14a1811ad
Richard Tingstad <richard.tingstad@amedia.no>
2021-03-28 10:26:36 +0200

    TCR

--- main_test.go
+++ main_test.go
@@ -152,13 +152,13 @@ func TestCursorPosition(t *testing.T) {
 func TestCursorPosition2(t *testing.T) {
 	string := ""
 	for i := 0; i >= 0; i-- {
-		string += "\x1b[2;Ho\n"
+		string += "\x1b[2;2Ho\n"
 	}
 	lines := CaptureReader(strings.NewReader(string))
 
 	got := strings.Join(lines, "\n")
 	want := `
-o`
+ o`
 	if got != want {
 		t.Errorf("Want:\n%s\ngot:\n%s", want, got)
 	}
