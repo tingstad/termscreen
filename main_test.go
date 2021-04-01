86dbf7e31a4c8474ad6e83efff18a22eb7f0a81e
Richard Tingstad <richard.tingstad@amedia.no>
2021-04-01 19:25:43 +0200

    TCR

--- main_test.go
+++ main_test.go
@@ -226,7 +226,7 @@ func TestEraseInDisplayToEndEmpty(t *testing.T) {
 }
 
 func TestEraseInDisplayToBeginningEmpty(t *testing.T) {
-	str := "\x1b[0J\n"
+	str := "\x1b[1J\n"
 	lines := strings.Join(CaptureReader(strings.NewReader(str)), "\n")
 
 	got := lines
