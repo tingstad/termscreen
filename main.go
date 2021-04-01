0f098781ad214ed1e83e7a3d62f769df430ecc37
Richard Tingstad <richard.tingstad@amedia.no>
2021-04-01 18:32:26 +0200

    TCR

--- main.go
+++ main.go
@@ -85,6 +85,7 @@ func Capture(reader MyReader) []string {
 						} else {
 							x = Max(0, count-1)
 						}
+					case "J": // Erase in display
 					case "K": // Erase in line
 						if count == 2 {
 							screen[y] = ""
