5a0763593bfc7b490d79ac34739d2e44a0530426
Richard Tingstad <richard.tingstad@amedia.no>
2021-04-01 19:10:48 +0200

    TCR

--- main_test.go
+++ main_test.go
@@ -214,12 +214,12 @@ func TestEraseInDisplay(t *testing.T) {
 	}
 }
 
-func TestEraseInDisplayToBeginning(t *testing.T) {
-	str := "Hello,\nHello, world \x1b[7D\x1b[2J\n"
+func TestEraseInDisplayToEnd(t *testing.T) {
+	str := "Hello, world \x1b[7D\x1b[0J\n"
 	lines := strings.Join(CaptureReader(strings.NewReader(str)), "\n")
 
 	got := lines
-	want := ""
+	want := "Hello,"
 	if got != want {
 		t.Errorf("Want:\n%s\ngot:\n%s", want, got)
 	}
