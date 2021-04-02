c03bdffc9148146b93065ecbff654dc00e6821ae
Richard Tingstad <richard.tingstad@amedia.no>
2021-04-03 00:21:26 +0200

    TCR

--- main.go
+++ main.go
@@ -152,7 +152,7 @@ func Print(screen []string, text string, x int, y int) []string {
 			if x < Len(screen[y]) {
 				prefix = screen[y][0:Pos(screen[y], x)]
 			} else {
-				prefix = screen[y] + strings.Repeat(" ", Pos(screen[y], x)-Len(screen[y]))
+				prefix = screen[y] + strings.Repeat(" ", x-Len(screen[y]))
 			}
 		}
 		screen[y] = prefix + text + suffix
