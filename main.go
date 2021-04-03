e899887446716d4696f77e9398215a19388ca433
Richard Tingstad <richard.tingstad@amedia.no>
2021-04-03 09:39:22 +0200

    TCR

--- main.go
+++ main.go
@@ -188,12 +188,6 @@ func Len(value string) int {
 	return RuneLen(value)
 }
 
-func ByteLen(value string) int {
-	re := regexp.MustCompile("\x1b\\[[0-9;]*[A-Za-z]")
-	stripped := re.ReplaceAll([]byte(value), []byte(""))
-	return len(stripped)
-}
-
 func RuneLen(value string) int {
 	re := regexp.MustCompile("\x1b\\[[0-9;]*[A-Za-z]")
 	stripped := string(re.ReplaceAll([]byte(value), []byte("")))
