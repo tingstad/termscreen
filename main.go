04417b13dece31ade7e8ab0dd0b5e4e102c99d86
Richard Tingstad <richard.tingstad@amedia.no>
2021-04-04 17:53:23 +0200

    TCR

--- main.go
+++ main.go
@@ -126,6 +126,7 @@ func Capture(reader MyReader) []string {
 								x = 0
 							}
 						}
+						fmt.Printf("%d %d\n", terminal.x, terminal.y)
 					}
 					if len(text) > indices[1] {
 						text = text[indices[1]:]
