023874fe81ad394f100dffa59b4a4855ad523163
Richard Tingstad <richard.tingstad@amedia.no>
2021-04-04 17:35:55 +0200

    TCR

--- main.go
+++ main.go
@@ -148,6 +148,9 @@ func Capture(reader MyReader) []string {
 	return screen
 }
 
+func PrintTerm(terminal *Terminal, screen []string, text string, x int, y int) {
+
+}
 func Print(screen []string, text string, x int, y int) []string {
 	for y >= len(screen) {
 		screen = append(screen, "")
