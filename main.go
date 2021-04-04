d669e9fa7c393fcb53b5ab6922913a5c9f2c44f3
Richard Tingstad <richard.tingstad@amedia.no>
2021-04-04 18:36:34 +0200

    TCR

--- main.go
+++ main.go
@@ -86,66 +86,6 @@ func (terminal *Terminal) HandleLine(re *regexp.Regexp, line string) {
 			terminal.HandleCode(countStart, countEnd, codeStart, codeEnd, count, codes, code)
 			screen = terminal.screen
 			x, y = terminal.x, terminal.y
-			switch code {
-			case "A": // Up
-				y = Max(0, y-count)
-			case "B": // Down
-				y += count
-			case "C": // Forward
-				x += count
-			case "D": // Back
-				x = Max(0, x-count)
-			case "E": // Next line
-				y += count
-				x = 0
-			case "F": // Previous line
-				y -= count
-				x = 0
-			case "G": // Column
-				if countEnd == countStart {
-					x = 1
-				} else {
-					x = Max(0, count-1)
-				}
-			case "J": // Erase in Display
-				idx := Pos(screen[y], x)
-				if count == 0 || countEnd == countStart { // To end
-					if Len(screen[y]) > x {
-						screen[y] = screen[y][0:idx]
-					}
-					screen = screen[0 : y+1]
-				} else if count == 1 { // To begining
-					screen[y] = strings.Repeat(" ", x) + screen[y][idx:]
-					for idx := range screen[0:y] {
-						screen[idx] = ""
-					}
-				} else if count > 1 { // All
-					screen = screen[:0]
-					x = 0
-					y = 0
-				}
-			case "K": // Erase in Line
-				idx := Pos(screen[y], x)
-				if count == 0 || countEnd == countStart { // To end
-					screen[y] = screen[y][0:idx]
-				} else if count == 1 { // To beginning
-					screen[y] = strings.Repeat(" ", x) + screen[y][idx:]
-				} else if count == 2 { // All
-					screen[y] = ""
-				}
-			default:
-				if codes[len(codes)-1:] == "H" { // Position
-					y = Max(0, count-1)
-					if codes[0:1] == ";" {
-						codes = codes[1:]
-					}
-					if len(codes) > 1 {
-						x = Max(0, Number(codes[0:len(codes)-1])-1)
-					} else {
-						x = 0
-					}
-				}
-			}
 			if len(text) > indices[1] {
 				text = text[indices[1]:]
 			} else {
@@ -164,7 +104,66 @@ func (terminal *Terminal) HandleLine(re *regexp.Regexp, line string) {
 func (terminal *Terminal) HandleCode(countStart, countEnd, codeStart, codeEnd, count int, codes, code string) {
 	screen := terminal.screen
 	x, y := terminal.x, terminal.y
-	Use(screen, x, y)
+	switch code {
+	case "A": // Up
+		y = Max(0, y-count)
+	case "B": // Down
+		y += count
+	case "C": // Forward
+		x += count
+	case "D": // Back
+		x = Max(0, x-count)
+	case "E": // Next line
+		y += count
+		x = 0
+	case "F": // Previous line
+		y -= count
+		x = 0
+	case "G": // Column
+		if countEnd == countStart {
+			x = 1
+		} else {
+			x = Max(0, count-1)
+		}
+	case "J": // Erase in Display
+		idx := Pos(screen[y], x)
+		if count == 0 || countEnd == countStart { // To end
+			if Len(screen[y]) > x {
+				screen[y] = screen[y][0:idx]
+			}
+			screen = screen[0 : y+1]
+		} else if count == 1 { // To begining
+			screen[y] = strings.Repeat(" ", x) + screen[y][idx:]
+			for idx := range screen[0:y] {
+				screen[idx] = ""
+			}
+		} else if count > 1 { // All
+			screen = screen[:0]
+			x = 0
+			y = 0
+		}
+	case "K": // Erase in Line
+		idx := Pos(screen[y], x)
+		if count == 0 || countEnd == countStart { // To end
+			screen[y] = screen[y][0:idx]
+		} else if count == 1 { // To beginning
+			screen[y] = strings.Repeat(" ", x) + screen[y][idx:]
+		} else if count == 2 { // All
+			screen[y] = ""
+		}
+	default:
+		if codes[len(codes)-1:] == "H" { // Position
+			y = Max(0, count-1)
+			if codes[0:1] == ";" {
+				codes = codes[1:]
+			}
+			if len(codes) > 1 {
+				x = Max(0, Number(codes[0:len(codes)-1])-1)
+			} else {
+				x = 0
+			}
+		}
+	}
 	terminal.x = x
 	terminal.y = y
 	terminal.screen = screen
