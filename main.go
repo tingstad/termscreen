f492685ae2d11f2e6667e8fdb1b872ebffb5fb66
Richard Tingstad <richard.tingstad@amedia.no>
2021-04-06 09:22:44 +0200

    TCR

--- main.go
+++ main.go
@@ -172,6 +172,7 @@ func (terminal *Terminal) PrintTerm(text string) {
 		for i := len(styles) - 1; i >= 0; i-- {
 			if styles[i] == "\x1b[0m" || styles[i] == "\x1b[m" {
 				styles = styles[i:]
+				break
 			}
 		}
 		terminal.style += strings.Join(styles, "")
