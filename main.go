aca38f461f5acfd043d0c8bd02b8378782eaa55c
Richard Tingstad <richard.tingstad@amedia.no>
2021-04-06 09:39:01 +0200

    TCR

--- main.go
+++ main.go
@@ -171,6 +171,8 @@ func (terminal *Terminal) PrintTerm(text string) {
 	if styles != nil {
 		for i := len(styles) - 1; i >= 0; i-- {
 			if styles[i] == "\x1b[0m" || styles[i] == "\x1b[m" {
+				if len(terminal.style) > 2 {
+				}
 				styles = styles[i:]
 				break
 			}
