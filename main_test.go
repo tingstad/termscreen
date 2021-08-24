ebb8094a51ff0545ed1453cc3aac88b09375486a
Richard Tingstad <richard.tingstad@gmail.com>
2021-08-24 16:17:28 +0200

    TCR

--- main_test.go
+++ main_test.go
@@ -337,6 +337,7 @@ func TestResetCode(t *testing.T) {
 	assertResetCode(true, "\x1b[m")
 	assertResetCode(true, "\x1b[0m")
 	assertResetCode(true, "\x1b[1;m")
+	assertResetCode(true, "\x1b[;0;m")
 	assertResetCode(false, "")
 	assertResetCode(false, "foo")
 	assertResetCode(false, "[0m")
