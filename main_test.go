c5532d2607ad1912fd332b96b6156934b08d2a39
Richard Tingstad <richard.tingstad@gmail.com>
2021-08-24 15:03:36 +0200

    TCR

--- main_test.go
+++ main_test.go
@@ -317,7 +317,6 @@ func TestUpdateStyle(t *testing.T) {
 	AssertEqualsStr(t, "", UpdateStyle("", []string{}))
 	AssertEqualsStr(t, "\x1b[33m", UpdateStyle("\x1b[33m", []string{}))
 	AssertEqualsStr(t, "\x1b[33m", UpdateStyle("\x1b[33m", []string{""}))
-	AssertEqualsStr(t, "\x1b[33m", UpdateStyle("\x1b[33m", []string{""}))
 	AssertEqualsStr(t, "\x1b[33m", UpdateStyle("", []string{"\x1b[33m"}))
 }
 
