73aee9f3fe7d331ad699e52c28f58d70ccb709ea
Richqrd <richard.tingstad@gmail.com>
2023-08-01 15:44:06 +0200

    copyright

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,10 @@
+// Copyright (C) 2021-2023 Richard H. Tingstad
+// This program is free software: you can redistribute it and/or modify it under the terms of the
+// GNU General Public License as published by the Free Software Foundation, version 3.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+
 package main
 
 import (
